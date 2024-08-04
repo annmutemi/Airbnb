fb632f2e32cde1b94b6acfb351d642806282eb47
annmutemi <annshiru92@gmail.com>
2024-08-04 17:13:09 +0300

    Navbar bar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let url = 'http://localhost:3000/airbnbs'
-const listHolder = document.getElementById('films')
+const listHolder = document.getElementById('airbnbs')
 
 window.addEventListener("load", function(){
     fetch(url).then(response => response.json())
@@ -11,8 +11,27 @@ window.addEventListener("load", function(){
 })
 
 function displayAirbnbs(airbnb){
+    const img = document.createElement('img')
+    img.className = "images"
+    img.setAttribute('src', airbnb.image)
+    img.setAttribute('width', "300")
+    img.setAttribute('height', "200")
+
+
+
+    const description = document.createElement("p")
+    description.className = ""
+    description.textContent = airbnb.description
+    
     const li = document.createElement('li')
-    li.style.cursor="pointer"
-    li.textContent= (airbnb.title).toUpperCase()
+    li.appendChild(img)
+    li.appendChild(description)
+    // li.textContent= (airbnb.title).toUpperCase()
+
+    // const taskText = document.createElement('span');
+    // taskText.textContent = airbnb.available;
+    // listHolder.appendChild(taskText)
+
+    listHolder.style.cursor = "pointer"
     listHolder.appendChild(li)
 }
